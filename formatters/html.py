eb576289d6bbca572f60c6eda89b54edfd139464
Shivnaren Srinivasan <shivnaren@gmail.com>
2024-04-08 21:27:00 +0530

    Remove pandas html tags from output str.
    Deprecate old method to generate html.

--- formatters/html.py
+++ formatters/html.py
@@ -1,17 +1,18 @@
 """HTML formatting utilities."""
 import re
 import pandas as pd
+import warnings
 
 
 def dataframe(df: pd.DataFrame, index: bool = True) -> str:
     """Return raw html table without formatting."""
+    warnings.warn('use `remove_pandas_tags` instead', DeprecationWarning)
+    return remove_pandas_tags(df.to_html(index=index))
 
-    html_tbl = re.sub(
-        r'<tr.*>',
-        '<tr>',
-        df.to_html(index=index).replace('border="1" ', ''),
-    )
-    return html_tbl
+
+def remove_pandas_tags(html: str) -> str:
+    """Remove pandas tags from generated html output."""
+    return re.sub(r'<tr.*>', '<tr>', html.replace('border="1"', ''))
 
 
 def hyperlink(text: str, url: str) -> str:
